8ffbac24dc20c9425a1b2e2a8333423c70ebb6e3
Jonas Hamm <contact@jonashamm.de>
2017-01-25 12:07:46 +0100

    add antiCacheString for css and js files add gulp jpg resizer task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-// NPM Modules //////////////////////////////////////////////////
+/********************************************* NPM Modules **********************************************/
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	cssnano = require('gulp-cssnano'),
@@ -6,14 +6,16 @@ var gulp = require('gulp'),
 	rename = require('gulp-rename'),
 	svgmin = require('gulp-svgmin'),
 	uglify = require('gulp-uglify'),
+	replace = require('gulp-replace'),
 	autoprefixer = require('gulp-autoprefixer'),
+	imageResize = require('gulp-image-resize'),
 	browserSync = require('browser-sync').create();
 
-// Custom folder variables //////////////////////////////////////
+/********************************************* Custom folder variables ***********************************/
 var folderSrc = 'src/',
 	folderDist = 'dist/';
 
-// Tasks ////////////////////////////////////////////////////////
+/********************************************* Default Tasks *********************************************/
 
 gulp.task('browser-sync', function() {
 	browserSync.init({
@@ -26,14 +28,14 @@ gulp.task('sass', function() {
 	return gulp.src([
 		'node_modules/normalize.css/normalize.css',
 		folderSrc + 'styles/custom.scss'
-		])
+	])
 		.pipe(concat('all-styles.scss'))
 		.pipe(sass({style: 'compressed'}).on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
-		.pipe(cssnano()) // disable for development with big files
+		// .pipe(cssnano()) // disable for development with big files
 		.pipe(rename({
 			suffix: ".min"
 		}))
@@ -43,11 +45,10 @@ gulp.task('sass', function() {
 
 gulp.task('compileJS',function() {
 	return gulp.src( [
-		'node_modules/somescript.min.js',
 		folderSrc + 'js/custom.js'
 	])
 		.pipe(concat('all-scripts.js'))
-		.pipe(uglify())
+		// .pipe(uglify())
 		.pipe(rename({
 			suffix: '.min'
 		}))
@@ -61,12 +62,42 @@ gulp.task('svgmin', function () {
 		.pipe(gulp.dest(folderDist + 'img/ui/'))
 });
 
-gulp.task('watch', function() {
-	gulp.watch( folderSrc  + 'js/!**.*', ['compileJS']);
-	gulp.watch( folderSrc  + 'styles/**.*', ['sass']);
-	gulp.watch( folderSrc + 'img/ui/*.svg', ['svgmin']);
-	gulp.watch("*.html").on('change', browserSync.reload);
+gulp.task('antiCache', function () {
+	return gulp.src('src/markup/index.php')
+		.pipe(replace('antiCacheString', Date.now()))
+		.pipe(gulp.dest('dist'));
 });
 
-// Default Task
-gulp.task('default', ['browser-sync', 'sass', 'compileJS', 'svgmin', 'watch']);
+gulp.task('watch', function() {
+	gulp.watch( folderSrc  + 'js/**.*', ['compileJS','antiCache']);
+	gulp.watch( folderSrc  + 'styles/**.*', ['sass','antiCache']);
+	gulp.watch( folderSrc + 'img/ui/*.svg', ['svgmin']);
+	gulp.watch("src/markup/*.php", ['antiCache']).on('change', browserSync.reload);
+});
+
+// Type in gulp on terminal/console to start standard tasks
+gulp.task('default', ['browser-sync', 'sass', 'antiCache', 'compileJS', 'svgmin', 'watch']);
+
+
+
+
+/********************************************* Special Tasks *********************************************/
+function makeJpgVersions($width,$height,$suffix) {
+	gulp.src('src/img/*.jpg')
+		.pipe(imageResize({
+			width : $width,
+			height : $height,
+			crop : true,
+			upscale : false
+		}))
+		.pipe(rename({
+			suffix: $suffix
+		}))
+		.pipe(gulp.dest('dist/img'));
+}
+
+gulp.task('images', function () {
+	makeJpgVersions(640,525,"-small");
+	makeJpgVersions(1280,570,"-middle");
+	makeJpgVersions(1920,670,"-big");
+});
---
+++ src/markup/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="dist/all-styles.min.css?version=antiCacheString">
+	<title>Project</title>
+</head>
+<body>
+<h1>Test Content</h1>
+<p>This is only test content.</p>
+
+<script src="dist/all-scripts.min.js?version=antiCacheString"></script>
+</body>
+</html>
