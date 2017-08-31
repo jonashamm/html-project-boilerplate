930bf14ed9745d86ae76ad0b6abaf99228ffad8b
Jonas H <contact@jonashamm.de>
2017-08-31 12:22:30 +0200

    Update index.php

--- src/markup/index.php
+++ src/markup/index.php
@@ -3,13 +3,25 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="dist/all-styles.min.css?version=antiCacheString">
+	<link rel="stylesheet" href="dist/all-styles.css?version=antiCacheString">
 	<title>Project</title>
 </head>
 <body>
-<h1>Test Content</h1>
-<p>This is only test content.</p>
-
-<script src="dist/all-scripts.min.js?version=antiCacheString"></script>
+	<header>
+		<div class="inner">
+			Header
+		</div>
+	</header>
+	<main>
+		<h1>Test Content</h1>
+		<p>This is only test content.</p>
+	</main>
+	<footer>
+		<div class="inner">
+			Footer
+		</div>
+	</footer>
+<script src="dist/vendor.combined.js?version=antiCacheString"></script>	
+<script src="dist/custom.compiled.js?version=antiCacheString"></script>
 </body>
 </html>
