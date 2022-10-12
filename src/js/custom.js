f3965fcb6fb4277d780af6a013a3f4d93f538cab
Jonas Hamm <contact@jonashamm.de>
2022-10-12 17:46:23 +0200

    update 2022

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,27 @@
+function navToggler() {
+	if (window.matchMedia("(max-width: 959px)").matches) {
+		$('.nav-main-toggler').click(function() {
+			$('.nav-main').slideToggle();
+		})
+		$('.toggle-subnav').click(function() {
+			$(this).closest('li').toggleClass('active').find('.nav-sub').slideToggle();
+		})
+		$('footer, main, .header-click-area').click(function () {
+			$('header, .nav-toggler').removeClass('nav-main-visible');
+			$('.nav-main').slideUp();
+		});
+	}
+}
 $(document).ready(function() {
+	navToggler();
 
+	var resizeTimer;
+	$(window).on('resize', function(e) {
+		clearTimeout(resizeTimer);
+		$('.nav-main-toggler, .toggle-subnav').unbind();
+		resizeTimer = setTimeout(function() {
+			console.log('resized');
+			navToggler();
+		}, 150);
+	});
 });
