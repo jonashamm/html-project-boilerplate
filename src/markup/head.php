bd9390a012b9188176272f5e07c40208e55b88fc
Jonas Hamm <contact@jonashamm.de>
2022-10-12 17:50:34 +0200

    update 2022

--- src/markup/head.php
+++ src/markup/head.php
@@ -3,14 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&Merriweather:wght@300;700;900&family=Montserrat:wght@100;400;600;700;800&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="dist/vendor-css.css?version=<?php echo $version;?>">
 		<link rel="stylesheet" href="dist/custom-compiled-from-sass1.css?version=<?php echo $version;?>">
-		<?php if ($_GET['version'] == 'light') : ?>
-			<link rel="stylesheet" href="dist/custom-compiled-from-sass.css?version=<?php echo $version;?>">
-		<?php endif; ?>
-		<title>Artists on the block</title>
+		<title>Project</title>
 	</head>
 	<body>
